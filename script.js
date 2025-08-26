d96484c38f75b9e01d0d0f019ec5859d1e6a1a7d
Mike A <115380567+Sprout7@users.noreply.github.com>
2025-08-26 17:12:24 -0400

    create html and js files and link them
    write pesudocode

---
+++ script.js
@@ -0,0 +1,12 @@
+// Psuedocode
+// 1. Write function getComputerChoice() that returns "rock", "paper", "scissors"
+// 2. Write player choice that return player choice of "rock", "paper", "scissors" using prompt
+// 3. Keep track of players score
+//  - compare the two choices
+//  - return a string declaing the round winner
+// 4. Keep track of score (player vs computer)
+// 5. Write function game() that plays the game five rounds
+//  - Each round, call playRound()
+//  - Update and show score
+//  - Announce overall winner
+console.log('Hello World');
